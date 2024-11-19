075e842f68bc1a7901fb1fdc616809711fe89e0a
AhmedALi <ahmedali27428@gmail.com>
2024-11-19 17:31:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -590,6 +590,8 @@ def load_and_combine_files(uploaded_files):
 # Function to perform scrubbing logic
 def scrub_data(needs_df, allskipped_df, scrub_on):
     # Convert standardized columns to lowercase
+ needs_df = needs_df.drop_duplicates(subset=['property_address', 'mailing_address'])
+ combined_df = combined_df.drop_duplicates(subset=['property_address', 'mailing_address'])
 
 
 
@@ -608,29 +610,29 @@ def scrub_data(needs_df, allskipped_df, scrub_on):
 
         needs_df['property_address'] = needs_df['property_address'].str.lower()
         allskipped_df['mailing_address'] = allskipped_df['mailing_address'].str.lower()
-        hits_df = needs_df.merge(allskipped_df, on=['mailing_address', 'property_address'], how='inner')
+        hits_df = needs_df.merge(combined_df, on=['property_address', 'mailing_address'], how='inner').drop_duplicates()
     elif scrub_on == 'Property Address':
         allskipped_df['property_address'].fillna('', inplace=True)
         needs_df['property_address'].fillna('', inplace=True)
         allskipped_df['property_address'] = allskipped_df['property_address'].apply(preprocess_address).apply(standardize_and_normalize_address).str.lower()
         needs_df['property_address'] = needs_df['property_address'].apply(preprocess_address).apply(standardize_and_normalize_address).str.lower()
-        hits_df = needs_df.merge(allskipped_df, on=['property_address'], how='inner')
+        hits_df = needs_df.merge(combined_df, on=['property_address'], how='inner').drop_duplicates(subset=['property_address'])
     elif scrub_on == 'Mailing Address':
         allskipped_df['mailing_address'].fillna('', inplace=True)
         needs_df['mailing_address'].fillna('', inplace=True)
         allskipped_df['mailing_address'] = allskipped_df['mailing_address'].apply(preprocess_address).apply(standardize_and_normalize_address).str.lower()
         needs_df['mailing_address'] = needs_df['mailing_address'].apply(preprocess_address).apply(standardize_and_normalize_address).str.lower()
-        hits_df = needs_df.merge(allskipped_df, on=['mailing_address'], how='inner')
+       hits_df = needs_df.merge(combined_df, on=['mailing_address'], how='inner').drop_duplicates(subset=['mailing_address'])
 
     # Remove matching records
     if scrub_on == 'Both':
         needs_df_filtered = needs_df[
-            ~needs_df[['mailing_address', 'property_address']].apply(tuple, axis=1).isin(
-                hits_df[['mailing_address', 'property_address']].apply(tuple, axis=1)
-            )
-        ]
+        ~needs_df[['property_address', 'mailing_address']].apply(tuple, axis=1).isin(
+            hits_df[['property_address', 'mailing_address']].apply(tuple, axis=1)
+        )
+    ]
     elif scrub_on == 'Property Address':
-        needs_df_filtered = needs_df[~needs_df['property_address'].isin(hits_df['property_address'])]
+          needs_df_filtered = needs_df[~needs_df['property_address'].isin(hits_df['property_address'])]
     elif scrub_on == 'Mailing Address':
         needs_df_filtered = needs_df[~needs_df['mailing_address'].isin(hits_df['mailing_address'])]
 
